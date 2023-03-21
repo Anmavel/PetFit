9a24367030fde6ccdb1b67b9c39db4ec9fcde9bd
anavelasquez <ana.maria.velasquez91@gmail.com>
2023-03-21 13:22:02 +0100

    Issue#1: fix bug in backend Add-Function for DTO : Override equals, hashCode and toString to consider array's content in the method

--- backend/src/main/java/com/example/backend/model/PetDTO.java
+++ backend/src/main/java/com/example/backend/model/PetDTO.java
@@ -1,7 +1,38 @@
 package com.example.backend.model;
 
-public record PetDTO( String name,
-                      String nameOfBreed,
-                      String photo,
-                      String[] supplies) {
+import java.util.Arrays;
+import java.util.Objects;
+
+public record PetDTO(String name,
+                     String nameOfBreed,
+                     String photo,
+                     String[] supplies) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PetDTO pet = (PetDTO) o;
+        return
+                Objects.equals(name, pet.name) &&
+                Objects.equals(nameOfBreed, pet.nameOfBreed) &&
+                Objects.equals(photo, pet.photo) &&
+                Arrays.equals(supplies, pet.supplies);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash( name, nameOfBreed, photo);
+        result = 31 * result + Arrays.hashCode(supplies);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                ", name='" + name + '\'' +
+                ", nameOfBreed='" + nameOfBreed + '\'' +
+                ", photo='" + photo + '\'' +
+                ", supplies=" + Arrays.toString(supplies) +
+                '}';
+    }
 }
