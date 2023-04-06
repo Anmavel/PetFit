9a8947e7b3fc0c02cbfe518642e3a0fcacda4f52
anavelasquez <ana.maria.velasquez91@gmail.com>
2023-04-06 17:30:39 +0200

    Issue#21-22: backend login logout

---
+++ backend/src/test/java/com/example/backend/exception/UnauthorizedExceptionTest.java
@@ -0,0 +1,28 @@
+package com.example.backend.exception;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UnauthorizedExceptionTest {
+    @Test
+    void testUnauthorizedExceptionWithDefaultMessage() {
+        UnauthorizedException exception = Assertions.assertThrows(UnauthorizedException.class,
+                () -> {
+                    throw new UnauthorizedException();
+                });
+
+        Assertions.assertEquals("You are not authorized!", exception.getMessage());
+    }
+
+    @Test
+    void testUnauthorizedExceptionWithCustomMessage() {
+        String message = "You are not authorized to access this Pet!";
+        UnauthorizedException exception = Assertions.assertThrows(UnauthorizedException.class,
+                () -> {
+                    throw new UnauthorizedException(message);
+                });
+
+        Assertions.assertEquals(message, exception.getMessage());
+    }
+
+}
